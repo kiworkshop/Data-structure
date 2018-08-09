8bec5cdc1c3e2efed3e58da98aea576a7c6da48f
Deocksoo <38533611+Deocksoo@users.noreply.github.com>
2018-08-10 01:07:00 +0900

    Coding convention과 관련된 feedback을 반영하여 코드를 재구성한다
    해당 커밋에서 feedback을 반영한 부분은 다음과 같다.
    
    1. 구글 스타일 가이드에 따른 띄어쓰기 일관성 확보
    2. 불필요한 공백, indent 제거
    3. 일부 변수 이름에서 불필요한 정보 제거하여 다시 작명

--- linked_list_basic.c
+++ linked_list_basic.c
@@ -1,160 +1,136 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node
-{
+struct Node {
     int element;
     struct Node *next;
 };
 
-struct Node *MakeNode(int element)
-{
-    struct Node *new_node_pointer = (struct Node *)malloc(sizeof(struct Node));
-    new_node_pointer->element = element;
-    new_node_pointer->next = NULL;
-    return new_node_pointer;
+struct Node *MakeNode(int element) {
+    struct Node *new = (struct Node *)malloc(sizeof(struct Node));
+    new->element = element;
+    new->next = NULL;
+    return new;
 }
 
-void MakeNewList(struct Node **initial_node)
-{
-    //Make element of header node -1
-    *initial_node = MakeNode(-1);
+void MakeNewList(struct Node **initial) {
+    *initial = MakeNode(-1);     //Make element of header node -1
 }
 
-struct Node *FindNode(struct Node *list, int key_element)
-{
-    struct Node *checked_node = list;
-
-    while ((checked_node != NULL) && (checked_node->element != key_element)){
-        checked_node = checked_node->next;
+struct Node *FindNode(struct Node *list, int key_element) {
+    struct Node *checked = list;
+    while ((checked != NULL) && (checked->element != key_element)) {
+        checked = checked->next;
     }
-
-    return checked_node;
+    return checked;
 }
 
-struct Node *FindPrevNode(struct Node *list, int key_element)
-{
-    if(FindNode(list, key_element) == NULL){
+struct Node *FindPrevNode(struct Node *list, int key_element) {
+    if (FindNode(list, key_element) == NULL) {
         return NULL;
     }
-    else{        
-        struct Node *checked_node = list;
+    else {        
+        struct Node *checked = list;
 
-        while((checked_node->next != NULL)&&(checked_node->next->element != key_element)){
-            checked_node = checked_node->next;
+        while ((checked->next != NULL) && (checked->next->element != key_element)) {
+            checked = checked->next;
         }
-        return checked_node;
+        return checked;
     }
 }
 
-void Insert(struct Node *list, int prev_node_element, int element)
-{
-    struct Node *prev_node = FindNode(list, prev_node_element);
-
-    if(prev_node){
-        struct Node *new_node = MakeNode(element);
-        new_node->next = prev_node->next;
-        prev_node->next = new_node;
+void Insert(struct Node *list, int prev_node_element, int element) {
+    struct Node *prev = FindNode(list, prev_node_element);
+    if (prev) {
+        struct Node *new = MakeNode(element);
+        new->next = prev->next;
+        prev->next = new;
     }
-    else{
+    else {
         printf("Insertion (%d) Failed : element %d is not in the list \n", element, prev_node_element);
     }
 }
 
-void Delete(struct Node *list, int key_element)
-{
-    struct Node *deleted_node = FindNode(list, key_element);
-    struct Node *prev_node = FindPrevNode(list, key_element);
+void Delete(struct Node *list, int key_element) {
+    struct Node *deleted = FindNode(list, key_element);
+    struct Node *prev = FindPrevNode(list, key_element);
     struct Node *temp;
-
-    if(!deleted_node){
+    if (!deleted) {
         printf("Deletion failed : element %d is not in the list.\n", key_element);
     }
-    else if(deleted_node == list){
+    else if (deleted == list) {
         printf("Deletion failed : Could not delete header.\n");
     }
-    else{
-        temp = deleted_node;
-        prev_node->next = deleted_node->next;
+    else {
+        temp = deleted;
+        prev->next = deleted->next;
         free(temp);
     }
 }
 
-int IsEmpty(struct Node *list)
-{
+int IsEmpty(struct Node *list) {
     return (list->next == NULL);
 }
 
-void PrintList(struct Node *list)
-{
-    struct Node *printed_node = list->next;
+void PrintList(struct Node *list) {
+    struct Node *printed = list->next;
     int index = 1;
-    
-    while(printed_node != NULL)
-    {
-        printf("[%d] key : %d    ", index++, printed_node->element);
-        printed_node = printed_node->next;
+    while (printed != NULL) {
+        printf("[%d] key : %d    ", index++, printed->element);
+        printed = printed->next;
     }
     printf("\n");
 
-    if(IsEmpty(list))
-    {
+    if (IsEmpty(list)) {
         printf("List is empty\n");
     }
 }
 
-void ExecuteBufferLine(struct Node *list, char *buffer)
-{
+void ExecuteBufferLine(struct Node *list, char *buffer) {
     char command;
     int new_element;
     int key_element;
-
     command = buffer[0];
 
-        if(command =='i'){
-            sscanf(buffer, "%c %d %d", &command, &new_element, &key_element);
-            Insert(list, key_element, new_element);
-        }
-        else if(command =='d'){
-            sscanf(buffer, "%c %d", &command, &key_element);
-            Delete(list, key_element);
-        }
-        else if(command =='f'){
-            sscanf(buffer, "%c %d", &command, &key_element);
-            struct Node *p = FindPrevNode(list, key_element);
-            if(p==list)
-                printf("Key of the previous node of %d is header\n", key_element);
-            else if(p==NULL)
-                printf("Could not find %d in the list\n", key_element);
-            else
-                printf("Key of the previous node of %d is %d\n", key_element, p->element);
-        }
-        else if(command =='p'){
-            PrintList(list);
-        }
-        else printf("Wrong instruction.\n");
+    if (command == 'i') {
+        printf("ok");
+        sscanf(buffer, "%c %d %d", &command, &new_element, &key_element);
+        Insert(list, key_element, new_element);
+    }
+    else if (command == 'd') {
+        sscanf(buffer, "%c %d", &command, &key_element);
+        Delete(list, key_element);
+    }
+    else if (command == 'f') {
+        sscanf(buffer, "%c %d", &command, &key_element);
+        struct Node *p = FindPrevNode(list, key_element);
+        if (p == list)
+            printf("Key of the previous node of %d is header\n", key_element);
+        else if (p == NULL)
+            printf("Could not find %d in the list\n", key_element);
+        else
+            printf("Key of the previous node of %d is %d\n", key_element, p->element);
+    }
+    else if (command == 'p') {
+        PrintList(list);
+    }
+    else {
+        printf("Wrong instruction.\n");
+    }
 }
 
-int main(int argc, const char *argv[])
-{
+int main(int argc, const char *argv[]) {
     struct Node *list;
     char buffer[10];
-
     MakeNewList(&list);
-
-    for(int i=0; i<argc; i++){
-         printf("%s\n", argv[i]);
-    }
     
-    FILE *fp = fopen(argv[1],"r");
-
-    while(1){
-        if(fgets(buffer, sizeof(buffer), fp)==NULL) break;
-        
+    FILE *fp = fopen(argv[1], "r");
+    while (1) {
+        if (fgets(buffer, sizeof(buffer), fp) == NULL)
+            break;
         ExecuteBufferLine(list, buffer);
     }
-
     fclose(fp);
-
+    
     return 0;
 }
