cbee41af80c22afc05262d7d92f960c190b74098
Deocksoo <38533611+Deocksoo@users.noreply.github.com>
2018-08-11 01:02:20 +0900

    필요한 Node를 찾는 함수들을 구현한다
    주어진 과제에서 Node를 찾을 때에는 student ID를 이용한다. 만약 student ID를 주어진 그대로
    이용한다면 string data끼리 같고 다름을 비교해야 하는데, 이는 무척 번거로운 작업이다. 따라서
    학번 string data를 각 학번마다 고유한 정수로 변환해주는 IdToInteger 함수를 정의하여 integer로
    만든 후에 비교할 수 있도록 하였다.

--- linked_list_application.c
+++ linked_list_application.c
@@ -20,6 +20,45 @@ void MakeList(struct Node **init) {
     *init = MakeNode("000000", NULL);
 }
 
+int IsEmpty(struct Node *list) {
+    return (list->next == NULL);
+}
+
+int IsLast(struct Node *node) {
+    return (node->next == NULL);
+}
+
+int IdToInteger(char *student_id) {
+    int transfered_id;
+    transfered_id = 100000 * student_id[0] + 10000 * student_id[1] + 1000 * student_id[2] + 100 * student_id[3] + 10 * student_id[4] + student_id[5];
+    return transfered_id;
+}
+
+struct Node *FindNode(struct Node *list, char *student_id) {
+    struct Node *checked = list;    //list의 header에서부터 찾기 시작
+    int integer_finding = IdToInteger(student_id);
+    while ((checked != NULL) && (IdToInteger(checked->student_id) != integer_finding)) {
+        checked = checked->next;
+    }
+    return checked;
+}
+
+struct Node *FindPrevNode(struct Node *list, char *student_id) {
+    if (IsEmpty(list)) {
+        return NULL;
+    }
+    else {
+        struct Node *checked = list;
+        int integer_finding = IdToInteger(student_id);
+        while (IdToInteger(checked->next->student_id) != integer_finding) {
+            checked = checked->next;
+            if (IsLast(checked)) 
+                return NULL;
+        }
+        return checked;
+    }
+}
+
 int main(int argc, const char *argv[]) {
     return 0;
 }
