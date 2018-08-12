01ac7c135db4098bc41d7e812bd1f494a403fab7
HyeongilNam <2080444@naver.com>
2018-08-12 23:48:44 +0900

    infix_to_postfix.c 피드백 반영 후 수정
    Stack이라는 구조체 선언시 띄어쓰기 반영하였고, 변수명 코딩 컨밴션 반영하였습니다. 그리고 하드코딩되어있는 stack size를 #define으로 따로 빼내었습니다. switch문, ++연산자, 동적할당 시 형변환 수정했고, 불필요한 주석 제거 및 if-else 문이 깊어지지 않도록 수정했습니다. <stdlib.h>에 비정상 종료 예외처리 반영하였습니다.

--- infix_to_postfix.c
+++ infix_to_postfix.c
@@ -6,94 +6,97 @@
 #include <ctype.h>
 
 #define DATACHAR 100
+#define MAX 100
 
 char target_line[DATACHAR];
 
-struct Stack{
+struct Stack {
     char* key;
     int top;
     int max_stack_size;
 };
 
-void MakeEmpty(struct Stack* S) {
-    S->top = -1;
-    S->max_stack_size = 40;
-    S->key =(char*)malloc(S->max_stack_size);
+void MakeEmpty(struct Stack* stack) {
+    stack->top = -1;
+    stack->max_stack_size = MAX;
+    stack->key =(char*)malloc(stack->max_stack_size);
 }
 
-int IsStackEmpty(struct Stack* S) {
-    return S->top == -1;
+int IsStackEmpty(struct Stack* stack) {
+    return stack->top == -1;
 }
 
 //우선순위 정하기
-int Priority(char operator){
+int Priority(char operator) {
     switch (operator) {
-      case '(': case')': 
-          return 3;         
-          break;
-      case '*': case'/': case'%':
-          return 2;
-          break;
-      case '+': case'-':
-          return 1;
-          break;
-      default:
-          return -1;
+        case '(': 
+        case ')': 
+            return 3;         
+            break;
+        case '*': 
+        case '/': 
+        case '%':
+            return 2;
+            break;
+        case '+': 
+        case '-':
+            return 1;
+            break;
+        default:
+            return -1;
     }
 }
 
-void Push(char word, struct Stack* S) {
-    S->top = S->top + 1;
-    S->key[S->top] = word;
+void Push(char word, struct Stack* stack) {
+    stack->top++;
+    stack->key[stack->top] = word;
 }
 
-void Pop(struct Stack* S, char* converted_line) {
-    if (S->key[S->top] == '(') {
-        S->top--;
+void Pop(struct Stack* stack, char* converted_line) {
+    if (stack->key[stack->top] == '(') {
+        stack->top--;
     }
     else {
-        strncat(converted_line, &S->key[S->top], 1);
-        S->top--;
+        strncat(converted_line, &stack->key[stack->top], 1);
+        stack->top--;
     }
 }
 
-      
-void InfixToPostfix(char* target_line, char* converted_line, struct Stack* S){
+void InfixToPostfix(char* target_line, char* converted_line, struct Stack* stack) {
     for (int i = 0; i < DATACHAR; i++) {
-        
         //피연산자와 연산자 구분 및 처리
         if (isdigit(target_line[i])) {
             strncat(converted_line, &target_line[i], 1);
+            continue;
+        }
+        
+        if (target_line[i] == '#') {
+            while (stack->top != -1) {
+                Pop(stack, converted_line);
+            }
+            continue;
+        }
+
+        if (stack->key[stack->top] == '(') {
+            Push(target_line[i], stack);
+            continue;
+        }
+
+        if (target_line[i] == ')') {
+            while(stack->key[stack->top] != '(') {
+                Pop(stack, converted_line);
+            }
+            Pop(stack,converted_line);
+            continue;
+        }
+
+        if (Priority(target_line[i] > Priority(stack->key[stack->top]))) {
+            Push(target_line[i], stack);
         }
         else {
-            int p = Priority(target_line[i]);
-            if (target_line[i] == '#') {
-                while (S->top != -1) { 
-                    Pop(S, converted_line);
-                }
-            }
-            else if (S->key[S->top] == '(') {
-                Push(target_line[i], S);
-            }
-            else if (target_line[i] == ')') {
-                while(S->key[S->top] != '(') {
-                    Pop(S, converted_line);
-                }
-                Pop(S,converted_line);
-            }
-            else {
-                //스택의 탑에 있는 문자 우선순위가 들어오는 문자의 우선순위보다 낮은 경우, 푸쉬
-                if (p > Priority(S->key[S->top])) {
-                    //test
-                    //printf("들어갈놈%c: %d, 스택안에 있는놈%c: %d\n",targetLine[i],p,S->key[S->top],Priority(S->key[S->top]));
-                    Push(target_line[i], S);
-                }
-                else {
-                    //스택의 탑에 있는 문자 우선순위가 같거나 낮은 연산자를 만나면 팝
-                    Pop(S, converted_line);
-                    Push(target_line[i], S);
-                }
-            }
+            //스택의 탑에 있는 문자 우선순위가 같거나 낮은 연산자를 만나면 팝
+            Pop(stack, converted_line);
+            Push(target_line[i], stack);
         }
     }
 }
@@ -105,19 +108,18 @@ int main(int argc, const char* argv[]) {
             char converted_line[DATACHAR];
             
             //스택만들고 초기화 설정
-            struct Stack* S;
-            S = malloc(sizeof(struct Stack));
-            MakeEmpty(S);
+            struct Stack* stack;
+            stack = (struct Stack*)malloc(sizeof(struct Stack));
+            MakeEmpty(stack);
 
             while (fgets(target_line, sizeof(target_line), fp)) {
-                InfixToPostfix(target_line, converted_line, S);
-
+                InfixToPostfix(target_line, converted_line, stack);
                 printf("original infix form : %s", target_line);
                 printf("converted postfix form : %s", converted_line);
             }
         } 
         else {
-            printf("input error!");
+            return EXIT_FAILURE;
         }
     }
    return 0;
