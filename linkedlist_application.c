44d6f4c6b9edfb2493f5a2f995b9aa653ea552c1
HyeongilNam <2080444@naver.com>
2018-08-13 23:19:30 +0900

    linkedlist_application.c파일 피드백 반영하여 수정
    enum과 struct 명 선언시 대문자로 수정하였습니다. 불필요한 중괄호 ,
    else문, 내용을 제거하여 수정하였습니다. 추가적으로 s_given_id 와 같은 모호한
    변수명을 tmp_given_id로 수정하였습니다.

--- linkedlist_application.c
+++ linkedlist_application.c
@@ -8,7 +8,7 @@
 #define MAX_BUF 128
 #define MAX_ARGUMENT 8
 
-enum command_t{
+enum Command{
     UNKNOWN,
     INSERT,
     DELETE,
@@ -16,9 +16,9 @@ enum command_t{
     PRINT,
 };
 
-struct commandline_t{
-    char buf[MAX_BUF];
-    enum command_t command;
+struct Commandline{
+    char tmp_string[MAX_BUF];
+    enum Command command;
     char student_id[MAX_ARGUMENT];     
     char student_name[MAX_ENROLLMENT];
 };
@@ -29,117 +29,98 @@ struct Node{
     struct Node* next;
 };
 
-void ParseCommandLine(struct commandline_t* commandline) {
-    char command = commandline->buf[0];
-
+void ParseCommandLine(struct Commandline *commandline) {
+    char command = commandline->tmp_string[0];
     switch (command) {
-      case 'i':
-          commandline->command = INSERT;
-          sscanf(commandline->buf, "%*c %s %[^\n]s", commandline->student_id, commandline->student_name);
+        case 'i':
+            commandline->command = INSERT;
+            sscanf(commandline->tmp_string, "%*c %s %[^\n]s", commandline->student_id, commandline->student_name);
+            break;
+        case 'd':
+            commandline->command = DELETE;
+            sscanf(commandline->tmp_string, "%*c %s", commandline->student_id);
+            break;
+        case 'f':
+            commandline->command = FIND;
+            sscanf(commandline->tmp_string, "%*c %s", commandline->student_id);
           break;
-      case 'd':
-          commandline->command = DELETE;
-          sscanf(commandline->buf, "%*c %s", commandline->student_id);
-          break;
-      case 'f':
-          commandline->command = FIND;
-          sscanf(commandline->buf, "%*c %s", commandline->student_id);
-          break;
-      case 'p':
-          commandline->command = PRINT;
-          break;
-      default:
-          commandline->command = UNKNOWN;
+        case 'p':
+            commandline->command = PRINT;
+            break;
+        default:
+            commandline->command = UNKNOWN;
     }
 }
 
-void MakeEmpty(struct Node* L);
-struct Node* Find(char* student_id,struct Node* L);
-void InsertF(char* student_id, char* student_name, struct Node* L);
-void PrintNode(struct Node* L);
-void Delete(char* student_id, struct Node* L);
+struct Node* CreateNode();
+struct Node* Find(char* student_id, struct Node* list);
+void InsertF(char* student_id, char* student_name, struct Node* list);
+void PrintNode(struct Node* list);
+void Delete(char* student_id, struct Node* list);
 
-int main(int argc, const char* argv[]){
+int main(int argc, const char* argv[]) {
     if (argc == 2) {
         FILE *fp = fopen(argv[1],"r");
         if (fp) {
-            struct commandline_t commandline;
-            
-            //make new node
-            struct Node* L = (struct Node*)malloc(sizeof(struct Node));
-
-            //initialize the node
-            MakeEmpty(L);    
-
-            while(fgets(commandline.buf, sizeof(commandline.buf),fp)) {
+            struct Commandline commandline;
+            struct Node* list = CreateNode();
+            struct Node* duplicate, *check, *find;
+            //여기 까지 수정
+            while(fgets(commandline.tmp_string, sizeof(commandline.tmp_string), fp)) {
                 ParseCommandLine(&commandline);
                 switch (commandline.command) {
-                  case INSERT:
-                      {
-                       //duplicate check with Find function
-                           struct Node* duplicate;
-                           duplicate = Find(commandline.student_id, L);
-                           if (duplicate != NULL) {
-                               printf("There already is an element with key %s . Insertion failed\n", commandline.student_id);
-                               break;
-                           }
-                           else {
-                           //Insertion function
-                               InsertF(commandline.student_id, commandline.student_name, L);
-                           }
-                           
-                           //Print all node
-                           printf("Insertion Success : %s \n", commandline.student_id);
-                           printf("Current List>");
-                           PrintNode(L);
-                           printf("\n");
-                           break;
-                       }
-                  case DELETE:
-                      {
-                           struct Node* check;
-                           check = Find(commandline.student_id, L);
-                           if (check == NULL) {
-                               printf("Deletion failed : element %s is not in the list \n", commandline.student_id);
-                               break;
-                           }
-                           else {
-                               //Delete function
-                               Delete(commandline.student_id, L);
+                    case INSERT:
+                        //duplicate check with Find function
+                        duplicate = Find(commandline.student_id, list);
+                        if (duplicate != NULL) {
+                            printf("There already is an element with key %s . Insertion failed\n", commandline.student_id);
+                            break;
+                        }
+                       
+                        //Insertion function
+                        InsertF(commandline.student_id, commandline.student_name, list);
+                        
+                        //Print all node
+                        printf("Insertion Success : %s \n", commandline.student_id);
+                        printf("Current List>");
+                        PrintNode(list);
+                        break;            
+                    case DELETE:
+                        check = Find(commandline.student_id, list);
+                        if (check == NULL) {
+                            printf("Deletion failed : element %s is not in the list \n", commandline.student_id);
+                            break;
+                        }
+                        else {
+                            //Delete function
+                            Delete(commandline.student_id, list);
 
-                               //Print all node
-                               printf("Deletion Success : %s\n", commandline.student_id);
-                               printf("Current List>");
-                               PrintNode(L);
-                               printf("\n");
-                               break;
-                            }
-                      }
-                  case FIND:
-                      {
-                           //FIND
-                           struct Node* find;
-                           find = (commandline.student_id, L);
-                           if (find == NULL) {
-                               printf("Find failed : element %s is not in the list", commandline.student_id);
-                               break;
-                           } 
-                           else {
-                               //Print the node
-                               printf("Find Success : %s\n", find->given_id);
-                               break;
-                            }
-                      }
-
-                  case PRINT:
-                      {
-                           //Print all node
-                           PrintNode(L);
-                           printf("\n");
-                           break;
-                      }
-                  default:
-                      continue;
+                            //Print all node
+                            printf("Deletion Success : %s\n", commandline.student_id);
+                            printf("Current List>");
+                            PrintNode(list);
+                            printf("\n");
+                            break;
+                        }
+                    case FIND:
+                        //FIND
+                        find = (commandline.student_id, list);
+                        if (find == NULL) {
+                            printf("Find failed : element %s is not in the list", commandline.student_id);
+                            break;
+                        } 
+                        else {
+                            //Print the node
+                            printf("Find Success : %s\n", find->given_id);
+                            break;
+                        }
+                    case PRINT:
+                        //Print all node
+                        PrintNode(list);
+                        printf("\n");
+                        break;
+                    default:
+                        continue;
                 }
             }
         }
@@ -150,40 +131,38 @@ int main(int argc, const char* argv[]){
     return 0;
 }
 
-void MakeEmpty(struct Node* L) {
-    L->given_id;
-    L->given_name;
-    L->next = NULL;
+struct Node* CreateNode() {
+    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+    new->next = NULL;
+    return new;
 }
 
-struct Node* Find(char* student_id, struct Node* L) {
-    struct Node* search = L->next;
-
-    int s_given_id = 1;
-    int s_stu_id = atoi(student_id);
+struct Node* Find(char* student_id, struct Node* list) {
+    struct Node* search = list->next;
+    int tmp_given_id;
+    int tmp_stu_id = atoi(student_id);
 
     while (search != NULL) {
-        s_given_id = atoi(search->given_id);
-        if (s_stu_id == s_given_id) {
+        tmp_given_id = atoi(search->given_id);
+        if (tmp_stu_id == tmp_given_id) {
             break;
         }
         else {
             search = search->next;
         }   
     }   
-
     return search;
 }
 
-void InsertF(char* student_id, char* student_name, struct Node* L) {
+void InsertF(char* student_id, char* student_name, struct Node* list) {
     //create new node
-    struct Node* tmp = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* tmp = CreateNode();
     strcpy(tmp->given_id, student_id);
     strcpy(tmp->given_name, student_name);
 
     int compare_target = atoi(student_id);
     //Insert with sorted position
-    struct Node* position = L;
+    struct Node* position = list;
     while (position->next != NULL) {
         int comparing = atoi(position->next->given_id);
 
@@ -204,20 +183,19 @@ void InsertF(char* student_id, char* student_name, struct Node* L) {
     }
 }
 
-void PrintNode(struct Node* L){
-    struct Node* position = L->next;
+void PrintNode(struct Node* list) {
+    struct Node* position = list->next;
     while (position != NULL) {
         printf("%s %s ", position->given_id, position->given_name);
         position = position->next;
     }
+    printf("\n");
 }
 
-void Delete(char* student_id, struct Node* L){
-    struct Node* position = L;
-
+void Delete(char* student_id, struct Node* list) {
+    struct Node* position = list;
     int compare_target = atoi(student_id);
-  
-    while (position->next != NULL) {
+    while (position->next) {
         int comparing = atoi(position->next->given_id);
         if (compare_target == comparing) {
             break;
@@ -226,7 +204,7 @@ void Delete(char* student_id, struct Node* L){
             position = position->next;
         }
     }
-    
+ 
     if (position->next->next == NULL) {
         free(position->next);
     }
