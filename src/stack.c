d03257a25bde689b090b33a6018ba5c03d0b791c
junsulime <vfv0121@gmail.com>
2018-08-14 00:29:53 +0900

    feat: Implement stack
    1. Push, Pop, Empty, ForEach, Free, FreeWithItems
    2. Dynamically allocated mem stack

--- src/stack.c
+++ src/stack.c
@@ -1,6 +1,63 @@
-#include <stdio.h>
 #include "stack.h"
+#include <stdlib.h>
 
-void hello() {
-    printf("hello world stack");
+struct Stack *Instance() {
+    struct Stack *stack = (struct Stack *)malloc(sizeof(struct Stack *));
+    stack->array = (void **)malloc(sizeof(void *) * INIT_STACK_SIZE);
+    stack->length = 0;
+    stack->array_size = INIT_STACK_SIZE;
+
+    return stack;
 }
+
+void Push(struct Stack *stack, void *element) {
+    if (stack->length == stack->array_size) {
+        stack->array = (void **)realloc(
+            stack->array, sizeof(void *) * (stack->array_size << 1));
+        stack->array_size = stack->array_size << 1;
+    }
+
+    stack->array[stack->length++] = element;
+}
+
+void *Pop(struct Stack *stack) {
+    if (stack->length > INIT_STACK_SIZE &&
+        stack->length < (stack->array_size >> 2)) {
+        stack->array = (void **)realloc(
+            stack->array, sizeof(void *) * (stack->array_size >> 1));
+        stack->array_size = stack->array_size >> 1;
+    }
+    return stack->array[--stack->length];
+}
+
+int Empty(struct Stack *stack) { return stack->length == 0; }
+
+void ForEach(struct Stack *stack, void (*Function)(void *data)) {
+    int i;
+    for (i = 0; i < stack->length; i++) {
+        Function(stack->array[i]);
+    }
+}
+
+void Free(struct Stack *stack) {
+    free(stack->array);
+    free(stack);
+}
+
+void FreeWithItems(struct Stack *stack) {
+    int i;
+    for (i = 0; i < stack->length; i++) {
+        free(stack->array[i]);
+    }
+    Free(stack);
+}
+
+const struct StackOp stack_op = {
+    .Instance = Instance,
+    .Push = Push,
+    .Pop = Pop,
+    .Empty = Empty,
+    .ForEach = ForEach,
+    .Free = Free,
+    .FreeWithItems = FreeWithItems,
+};
--- src/stack_main.c
+++ src/stack_main.c
@@ -1,5 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "stack.h"
 
+void PrintFunction(void *data) { printf("%d ,", *(int *)data); }
+
 int main() {
-    hello();
+    struct StackOp op = stack_op;
+
+    int i;
+    int element1 = 1;
+    int element2 = 2;
+    int element3 = 3;
+    int element4 = 4;
+
+    int *array = (int *)malloc(sizeof(int) * 1000);
+
+    struct Stack *stack = op.Instance();
+
+    printf("isEmpty: %d\n", op.Empty(stack));
+
+    // result: isEmpty: 1
+
+    op.Push(stack, &(element1));
+    op.Push(stack, &(element2));
+    op.Push(stack, &(element3));
+    void *data = op.Pop(stack);
+    printf("top: %d\n", *(int *)data);
+
+    // result: top: 3
+
+    op.Push(stack, &(element4));
+    op.ForEach(stack, PrintFunction);
+    printf("\n");
+
+    // result: 1, 2, 4
+
+    printf("isEmpty: %d\n", op.Empty(stack));
+
+    // result: isEmpty: 0
+
+    for (i = 0; i < 1000; i++) {
+        op.Push(stack, &array[i]);
+    }
+
+    for (i = 0; i < 1000; i++) {
+        op.Pop(stack);
+    }
+
+    printf("top: %d\n", *(int *)op.Pop(stack));
+
+    // result: top: 4
+
+    op.Free(stack);
+
+    return 0;
 }
